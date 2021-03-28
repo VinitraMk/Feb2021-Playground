8e8ada99a4360131252799473c00f5f3347deb9a
VinitraMk <vinitramk@gmail.com>
2021-03-28 12:48:10 +0000

    adding random forest with 0.876

--- models/random_forest.py
+++ models/random_forest.py
@@ -10,6 +10,6 @@ class RandomForest:
         self.y = y
 
     def get_model(self):
-        model = RandomForestRegressor(n_estimators=800, max_features='log2', max_depth=20, min_samples_split=2, random_state=42, n_jobs=-1)
+        model = RandomForestRegressor(n_estimators=800, max_features='log2', max_depth=15, min_samples_split=2, random_state=42, n_jobs=-1)
         model.fit(self.X,self.y)
         return model
